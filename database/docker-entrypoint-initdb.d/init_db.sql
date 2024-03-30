bf63b771ea5370d584f8e5d134745e73072b91fd
Никита Черешнев <nikitaceresnev@Nikitas-MacBook-Air.local>
2024-03-30 22:14:07 +0300

    added psql table and mini refactor NATS

---
+++ database/docker-entrypoint-initdb.d/init_db.sql
@@ -0,0 +1,13 @@
+-- auto-generated definition
+CREATE TABLE Employee (
+    EmployeeId INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Position VARCHAR(50),
+    Department VARCHAR(50),
+    HireDate DATE,
+    Salary INT,
+    Email VARCHAR(100),
+    PhoneNumber VARCHAR(20),
+    Address VARCHAR(255)
+);
