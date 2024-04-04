f74790cdc070da01065cb48eca109cb32e51ce00
Никита Черешнев <nikitaceresnev@Nikitas-MacBook-Air.local>
2024-04-04 23:00:27 +0300

    first try of read refactor

---
+++ reader/cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"./pkg/common/db"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	viper.SetConfigFile("./pkg/common/envs/.env")
+	viper.ReadInConfig()
+
+	port := viper.Get("PORT").(string)
+	dbUrl := viper.Get("DB_URL").(string)
+
+	r := gin.Default()
+	db.Init(dbUrl)
+
+	r.Get("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"port":  port,
+			"dbUrl": dbUrl,
+		})
+	})
+
+	r.Run(port)
+}
---
+++ reader/pkg/common/db/db.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+	"log"
+	"models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Init(url string) *gorm.DB {
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+
+	if err != nil {
+		log.Panic("Init db error: %v", err)
+	}
+
+	db.AutoMigrate(&models.Employee{})
+
+	return db
+}
